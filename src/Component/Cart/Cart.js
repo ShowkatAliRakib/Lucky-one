a5c6eafdbc256468d286f708b3f7f9ec6b2c84a2
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-03-26 18:00:10 +0600

    distracsaring

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,13 +4,12 @@ import React from 'react';
 import "./Cart.css"
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
-const Cart = (props) => {
-    const {name, price, img} = props.cart;
+const Cart = ({cart}) => {
+    const {name, price, img} = cart;
     return (
         <div className="cart">
-           <div className="img-container">
-           <img src={img} width="100%" alt="" srcset="" />
-           </div>
+            <img src={img} width="100%" alt="" srcset="" />
+          
             <h2>{name}</h2>
             <p>Price:{price}</p>
 
